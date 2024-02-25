28785dfe926694fd69c8553a283220628b67d1ba
Sphax <itssphax123@gmail.com>
2024-02-25 08:07:30 -0800

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 
 # Ensure SSH client configuration
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication',
+  ensure => 'present',
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/your_username/.ssh/config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => '^#?(\s*)IdentityFile',
+  ensure => 'present',
+  path   => '/home/your_username/.ssh/config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
 
