6182db31456d1bfcbdfbc8658dc12c91271a18b6
Sphax <itssphax123@gmail.com>
2024-04-09 16:04:45 -0700

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Top Ten"""
+import requests
+
+
+def top_ten(subreddit):
+    """ first 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'ALX-Reddit-Agent'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()['data']['children']
+        for post in data[:-1]:
+            print(post['data']['title'])
+    else:
+        print("None")
