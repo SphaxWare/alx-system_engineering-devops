5ccaa072fdb245351e8b36cb4614a4caa674f271
Sphax <itssphax123@gmail.com>
2024-04-09 15:26:27 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,15 +8,16 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'ALX-Reddit-Agent'}
     try:
-        print(url)
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, allow_redirects=True)
         if response.status_code == 200:
             data = response.json()
-            subcount = data['data']['subscribers']
+            subcount = 0
+            if 'subscribers' in data['data']:
+                subcount = data['data']['subscribers']
             return subcount
         else:
             print(f"Error: {response.status_code}")
             return 0
     except Exception as e:
         print(f"Error: {e}")
-        return None
+        return 0
