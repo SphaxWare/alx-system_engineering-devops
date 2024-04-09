662ed16798f33c45678b95abd46affe89d9aacec
Sphax <itssphax123@gmail.com>
2024-04-09 16:56:30 -0700

    recurse

--- 0x16-api_advanced/2-WWrecurse.py
+++ 0x16-api_advanced/2-WWrecurse.py
@@ -3,22 +3,16 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[]):
     """list containing the titles of all hot articles"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'ALX-Reddit-Agent'}
-    params = {'after': after}
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()['data']['children']
         if data:
             for post in data:
                 hot_list.append(post['data']['title'])
-            after = data[-1]['data']['name']
-            return recurse(subreddit, hot_list, after)
-        else:
-            if len(hot_list) == 0:
-                return None
             return hot_list
-    else:
-        return None
+        else:
+            return None
