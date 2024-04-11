603e96b0cb2896df3d130d15214490791b22e2cb
SphaxWare <134861101+SphaxWare@users.noreply.github.com>
2024-04-11 13:16:02 +0000

    Update 0-strace_is_your_friend.pp for puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # 0x17-debugging_3
 
 exec { 'Fix_wp-settings.php':
-  command => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
+  command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
   provider => shell,
 }
